7dc7356acf053b16f062bdf136f9ec0d962e3cbb
fsvymk <fsvymk@gmail.com>
2015-11-23 23:41:19 +0300

    Monday-night

--- graphs.cpp
+++ graphs.cpp
@@ -18,7 +18,64 @@ graphs::~graphs()
     delete ui;
 }
 
+// Events
+
 void graphs::on_pushButton_5_clicked()
 {
-    ui->tabWidget->addTab(); // there are error
+    //ui->tabWidget->addTab(new QWidget); // there are error
+}
+
+// Output:
+
+
+
+// Use cases:
+
+void graphs::init(){
+
+    connect(&udpServerSocket, SIGNAL(readyRead()),
+            this, SLOT(readUdpDatagrams()));
+    datagramCounter = 0;
+    datagramMaxSize = 0;
+
+    ui->customPlot->clearPlottables();
+    ui->customPlot->setLayout(new QVBoxLayout);
+    ui->customPlot->clearGraphs();
+
+    graphCount  = 20;   // max graph count;
+
+    plots   = new QCustomPlot[graphCount];
+    vbox    = (QVBoxLayout*)ui->customPlot->layout();
+}
+
+void graphs::readUdpDatagrams()
+{
+    while (udpServerSocket.hasPendingDatagrams()) {
+        QByteArray datagram;
+        datagram.resize(udpServerSocket.pendingDatagramSize());
+        QHostAddress sender;
+        quint16 senderPort;
+
+        udpServerSocket.readDatagram(datagram.data(), datagram.size(),
+                                &sender, &senderPort);
+
+        datagramCounter++;
+        ui->line_datagrams->setText(ss(datagramCounter));
+
+        if (datagram.size()>datagramMaxSize) datagramMaxSize = datagram.size();
+        ui->line_maxdata->setText(ss(datagramMaxSize));
+
+        //QHash<unsigned, struct entry> db;
+        entryMassDeserialize(&datagram, &db);
+
+        QString output = "QHash db ["+QString::number(db.count())+"].";
+        ui->textVectors->append(output);
+
+        //output_db(db);
+        //output_vectors(&_x,&_y);
+        //draw(&_x,&_y);
+        //numbers(db);
+
+        //realTime_Redraw();
+    }
 }
--- graphs.h
+++ graphs.h
@@ -6,6 +6,7 @@
 #include <QMap>
 #include <QtMath>
 
+#include <entry.h>
 #include <qcustomplot.h>
 
 namespace Ui {
@@ -21,10 +22,44 @@ public:
     ~graphs();
 
 private slots:
+    // event listeners:
     void on_pushButton_5_clicked();
+
+
+    // use cases:
+    void init();
+    void remove(QLayout* layout);
+    QString ss(long x);
+    void p(QString str);
+
+    // graphs:
+    void entryMassDeserialize(QByteArray* source,
+                              QHash<unsigned, struct entry>* DB);
+    void readUdpDatagrams();
+    void output_vectors(QVector<double> *x, QVector<double> *y);
+    void output_db(QHash<unsigned, struct entry> &db);
+    void numbers(QHash<unsigned, struct entry> &db);
+
+    //parser:
+    void displayError(int line);
+    void parseLine(const QString& text, int line, const QMap<QString, int> &sems);
+    void processScript(int line, QString type, QString value, QStringList numbers, const QMap<QString, int> &sems);
+    void processUdpDatagram(const QByteArray& data);
+    bool parseSem(QXmlStreamReader& xml, QMap<QString, int> &sems);
 
 private:
     Ui::graphs *ui;
+
+    // my properties
+    int cod;
+    QUdpSocket udpSocket;
+    QUdpSocket udpServerSocket;
+    QList<unsigned> graphT;
+    QHash<unsigned, struct entry> db;
+    quint32 datagramCounter;
+    quint32 datagramMaxSize;
+    QCustomPlot* plots;
+    QVBoxLayout* vbox;
 };
 
 #endif // GRAPHS_H
